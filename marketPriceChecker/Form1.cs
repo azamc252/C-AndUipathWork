ad271894407d53a4bd68ae46164106d22fc35359
azamc252 <50447124+azamc252@users.noreply.github.com>
2023-11-01 11:02:39 +0000

    Add files via upload

---
+++ marketPriceChecker/Form1.cs
@@ -0,0 +1,43 @@
+namespace marketPriceChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            metalsPrices1.Visible = true;
+            homePage1.Visible = false;
+            companyCheck1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            homePage1.Visible = true;
+            metalsPrices1.Visible = false;
+            companyCheck1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            metalsPrices1.Visible = false;
+            homePage1.Visible = false;
+            companyCheck1.Visible = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void homeBtn_Click(object sender, EventArgs e)
+        {
+            homePage1.Visible = true;
+            metalsPrices1.Visible = false;
+            companyCheck1.Visible=false;
+        }
+    }
+}
